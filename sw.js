26c062e56b89a7a709a5c7b995b5606b2a26f1e7
chongyv4-arch <chongyv4@gmail.com>
2025-11-01 12:39:43 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js
+const CACHE = 'box-v1';
+const FILES = [
+  '/box.html',
+  '/manifest.json',
+  '/sw.js'      // 把自己也缓存
+];
+
+// 安装时一次性把核心文件写进缓存
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+// 离线时先读缓存，没命中才走网络
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
